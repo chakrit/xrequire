bbf5a38cee13c2cc223b3a0ae2b23f688fba6843
Chakrit Wichian <chakrit@oozou.com>
2012-11-19 17:50:07 +0700

    Initial xrequire implementaion.

--- test.js
+++ test.js
@@ -2,16 +2,6 @@
 // test.js - Tests xrequire functionality
 module.exports = (function() {
 
-  // related files
-  var FILES =
-    { XREQUIRE: './xrequire'
-    };
-
-  // use files in cover/ folder in coverage mode
-  if (process.env.COVER)
-    for (key in FILES)
-      FILES[key] = './cover/' + FILES[key];
-
   // testing tools
   var _ = require('underscore')
     , chai = require('chai')
@@ -20,13 +10,191 @@ module.exports = (function() {
 
   chai.use(require('sinon-chai'));
 
+  // related files
+  // use files in cover/ folder in coverage mode
+  var XREQ_PATH = './xrequire'
+  if (process.env.COVER) {
+    XREQ_PATH = './cover/xrequire';
+
+    //expect(__coverage__).to.exist
+  }
+
   // the tests
   describe('xrequire module', function() {
-    before(function() {
-      this.xrequire = require(FILES.XREQUIRE);
+    before(function() { this.xrequire = require(XREQ_PATH); });
+    after(function() { delete this.xrequire; });
+
+    it('should exports a function', function() {
+      expect(this.xrequire).to.be.a('function');
     });
 
-    it('should be exported', function() { });
+    describe('defaults hash', function() {
+      before(function() { this.defs = this.xrequire.defaults });
+      after(function() { delete this.defs });
+
+      it('should be exported', function() {
+        expect(this.defs).to.be.an('object');
+      });
+
+      it('should have a prepend property which defaults to empty string', function() {
+        expect(this.defs).to.have.property('prepend').that.is.a('string');
+      });
+
+      it('should have an append property which defaults to empty string', function() {
+        expect(this.defs).to.have.property('append').that.is.a('string')
+      });
+
+      it('should have an inflect property which defaults to `identity`', function() {
+        expect(this.defs).to.have.property('inflect').that.is.a('string')
+      });
+
+      it('should have a filter property which defaults to null', function() {
+        expect(this.defs).to.have.property('filter').that.is.null
+      });
+
+      it('should have a reject property which defaults to null', function() {
+        expect(this.defs).to.have.property('reject').that.is.null
+      });
+
+      it('should have a map property which defaults to null', function() {
+        expect(this.defs).to.have.property('map').that.is.null
+      });
+    });
+
+    describe('exported function', function() {
+      it('should throws if module argument is missing', function() {
+        expect(function() { this.xrequire(false); }.bind(this)).to.throw(/argument/)
+      });
+
+      it('should throws if module argument is not a string and does not looks like a module', function() {
+        expect(function() { this.xrequire({ }); }.bind(this)).to.throw(/argument/)
+      });
+    });
+
+  });
+
+  describe('xrequire', function() {
+    before(function() {
+      this.xrequire = require('./xrequire')
+      this.run = function() {
+        return this.xrequire('./sample', this.options);
+      }.bind(this);
+    });
+    after(function() { delete this.run });
+
+    // save and restore require cache state
+    beforeEach(function() {
+      this.required = []
+      for (var key in require.cache)
+        this.required.push(key);
+    });
+
+    afterEach(function() {
+      for (var key in require.cache)
+        if (_(this.required).indexOf(key) === -1)
+          delete require.cache[key]
+    });
+
+
+    // test template
+    var itShouldExportTree = function(tree, badTree) {
+      for (var key in tree) (function(content, key) {
+        it('should include `' + key + '` module.', function() {
+          var result = this.run();
+          expect(result).to.have.property(key).that.is.eq(content);
+        });
+      })(require(tree[key]), key);
+    };
+
+    var itShouldNotExportKeys = function(keys) {
+      for (var i = 0; i < keys.length;i ++) (function(key) {
+        it('should not include `' + key + '` module.', function() {
+          var result = this.run();
+          expect(result).to.not.have.property(key);
+        });
+      })(keys[i]);
+    };
+
+    var describeConfiguration = function(desc, opts, expectedTree, unexpectedKeys) {
+      describe('result with ' + desc + ' options passed', function() {
+        before(function() { this.options = opts; });
+        after(function() { delete this.options; });
+
+        itShouldExportTree(expectedTree);
+        if (unexpectedKeys)
+          itShouldNotExportKeys(unexpectedKeys);
+      });
+
+      describe('result with ' + desc + ' defaults set', function() {
+        before(function() {
+          this._defaults = this.xrequire.defaults;
+          this.xrequire.defaults = opts;
+        });
+        after(function() {
+          this.xrequire.defaults = this._defaults;
+          delete this._defaults;
+        });
+
+        itShouldExportTree(expectedTree);
+        if (unexpectedKeys)
+          itShouldNotExportKeys(unexpectedKeys);
+      });
+    };
+
+    // test various configurations
+    describeConfiguration('no', undefined,
+      { a: './sample/a'
+      , b: './sample/b'
+      , long_name: './sample/long_name'
+      },
+      ['index']);
+
+    describeConfiguration('prepend', { prepend: 'PREFIX' },
+      { PREFIXa: './sample/a'
+      , PREFIXb: './sample/b'
+      , PREFIXlong_name: './sample/long_name'
+      });
+
+    describeConfiguration('append', { append: 'SUFFIX' },
+      { aSUFFIX: './sample/a'
+      , bSUFFIX: './sample/b'
+      , long_nameSUFFIX: './sample/long_name'
+      });
+
+    describeConfiguration('`titleize` inflect', { inflect: 'titleize' },
+      { A: './sample/a'
+      , B: './sample/b'
+      , "Long Name": './sample/long_name'
+      });
+
+    describeConfiguration('`dasherize` inflect', { inflect: 'dasherize' },
+      { a: './sample/a'
+      , b: './sample/b'
+      , "long-name": './sample/long_name'
+      });
+
+    describeConfiguration('full naming', { prepend: 'P', append: 'S', inflect: 'classify' },
+      { PAS: './sample/a'
+      , PBS: './sample/b'
+      , PLongNameS: './sample/long_name'
+      });
+
+    describeConfiguration('filter', { filter: function(name) { return name === 'a'; } },
+      { a: './sample/a' },
+      ['b', 'long_name']);
+
+    describeConfiguration('reject', { reject: function(name) { return name === 'b'; } },
+      { a: './sample/a'
+      , long_name: './sample/long_name'
+      },
+      ['b']);
+
+    describeConfiguration('map', { map: function(map) { return require('./dud'); } },
+      { a: './dud'
+      , b: './dud'
+      , long_name: './dud'
+      });
+
   });
 
 })();
--- xrequire.js
+++ xrequire.js
@@ -1,15 +1,64 @@
 
 // xrequire.js - Main xrequire loader.
-module.exports = function(mod, options) {
+module.exports = (function() {
 
   var _ = require('underscore')
-    , i = require('i')();
-
+    , i = require('i')()
+    , path = require('path')
+    , fs = require('fs');
 
   var defaults =
-    { inflector: i
-    , inflect: null
-    };
+    { filter: null
+    , reject: null
+    , map: null
 
-}
+    , prepend: ''
+    , append: ''
+    , inflect: ''
+    }
 
+
+  function xrequire(mod, options) {
+    if (!mod ||
+      (typeof mod !== 'string' && 
+      typeof mod.path !== 'string')) {
+      throw new Error('module argument must be the module object or __dirname');
+    }
+
+    options = _.extend({ }, xrequire.defaults, options);
+
+    var dirname = (typeof mod === 'string' ? mod : path.dirname(mod.path))
+      , files = fs.readdirSync(dirname)
+      , result = { };
+
+    _(files).forEach(function(file) {
+      var filePath = path.join(dirname, file)
+        , extension = path.extname(file)
+        , name = path.basename(file, extension);
+
+      // filters
+      if (name === 'index') return;
+      if (options.filter && !options.filter(name)) return;
+      if (options.reject && options.reject(name)) return;
+
+      // naming rules
+      if (options.inflect) name = i[options.inflect](name)
+      if (options.prepend) name = options.prepend + name;
+      if (options.append) name = name + options.append;
+
+      // maps
+      var mod = require('./' + filePath);
+      if (options.map)
+        mod = options.map(mod, name, path.basename(file, extension));
+
+      result[name] = mod;
+    });
+
+    return result;
+  }
+
+
+  xrequire.defaults = defaults;
+  return xrequire;
+
+})();
